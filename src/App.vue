ca4a1c02cdf357c99f3015854404a566c8190809
tateyama1009 <1416070@ed.tus.ac.jp>
2020-01-09 20:49:42 +0900

    a

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <VsCol vs-offset="1">
+      <br />
+      <h1>Lovely Vue</h1>
+      <br />
+      <VsRow>
+        <VsInput v-model="message" size="large" />
+        <VsButton :disabled="isDisabled" class="add_button" @click="add">
+          追加
+        </VsButton>
+      </VsRow>
+      <br />
+      <template v-if="todos.length === 0">
+        <VsRow>
+          タスクはありません
+        </VsRow>
+      </template>
+      <template v-else>
+        <VsRow v-for="todo in todos" :key="todo.id" vs-w="10">
+          <TaskCard :id="todo.id" :text="todo.text" @remove-task="remove" />
+        </VsRow>
+      </template>
+    </VsCol>
   </div>
 </template>
 
+<script>
+import TaskCard from '@/components/TaskCard.vue'
+export default {
+  name: 'App',
+  components: {
+    TaskCard
+  },
+  data: () => ({
+    todos: [{ id: 1, text: 'ときめき' }],
+    message: 'Spark joy!'
+  }),
+  computed: {
+    isDisabled () {
+      return this.message.length === 0
+    }
+  },
+  methods: {
+    add () {
+      const newTodo = {
+        id: this.todos.length + 1,
+        text: this.message
+      }
+      this.todos.push(newTodo)
+      this.message = ''
+    },
+    remove (id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.add_button {
+  margin-left: 8px;
 }
 </style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <VsCard>
+    <h3>{{ text }}</h3>
+    <div slot="footer">
+      <VsRow vs-justify="flex-end">
+        <VsButton icon="delete" color="gray" @click="remove"></VsButton>
+      </VsRow>
+    </div>
+  </VsCard>
+</template>
+
+<script>
+export default {
+  name: 'TaskCard',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    remove () {
+      // todo:削除の実装
+      console.log(`remove: ${this.id}`)
+      this.$emit('remove-task', this.id)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
